c6942c67c9cfdf9b2c66432e6cc1c2c2cef46728
Georgian Grec <georgian.grec@gmail.com>
2018-07-02 14:05:29 +0300

    Got a custom instantsearch store to work!

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,38 @@
 <template>
-<!--  <ais-index
-    index-name="vdf"
-    :search-client="searchClient"
-  >-->
   <ais-index
-    app-id="latency"
-    api-key="3d9875e51fbd20c7754e65422f7ce5e1"
-    index-name="bestbuy"
-  >
-    <v-app id="vdf">
-      <v-toolbar color="amber" app absolute clipped-left>
-        <span class="title ml-3 mr-5">Varf De Forma</span>
-        <ais-search-box>
-          <!--<v-text-field
-            solo-inverted
-            flat
-            label="Search"
-            prepend-icon="search"
-          ></v-text-field>-->
-        </ais-search-box>
+    index-name='vdf'
+    :search-store='searchStore'>
+    <v-app id='vdf'>
+      <v-toolbar color='amber' app absolute clipped-left>
+        <span class='title ml-3 mr-5'>Varf De Forma</span>
+        <div id='search-box'>
+          <!-- SearchBox widget will appear here -->
+        </div>
         <v-spacer></v-spacer>
       </v-toolbar>
       <v-content>
-        <v-container fluid fill-height class="grey lighten-4">
+        <v-container fluid fill-height class='grey lighten-4'>
+
+          <v-layout row wrap>
+            <v-flex d-flex xs12 order-xs5>
+              <v-layout column>
+                <v-flex d-flex>
+                  <h1>Name</h1>
+                  <ais-refinement-list attribute-name='sport'></ais-refinement-list>
+                </v-flex>
+                <v-flex>
+                  <h1>Discipline</h1>
+                  <ais-refinement-list attribute-name='discipline'></ais-refinement-list>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+
           <v-layout justify-center align-center>
             <ais-results>
-              <template slot-scope="{ result }">
+              <template slot-scope='{ result }'>
                 <h2>
-                  <ais-highlight :result="result" attribute-name="name"></ais-highlight>
+                  <ais-highlight :result='result' attribute-name='name'></ais-highlight>
                 </h2>
               </template>
             </ais-results>
@@ -39,17 +44,81 @@
 </template>
 
 <script>
+import { createFromAlgoliaClient } from 'vue-instantsearch'
+
+// const result = {
+//   'results': [{
+//     'exhaustiveNbHits': true,
+//     'hits': [
+//       {
+//         'name': 'a',
+//         '_highlightResult': {
+//           'name': 'a'
+//         }
+//       },
+//       {
+//         'name': 'b',
+//         '_highlightResult': {
+//           'name': 'b'
+//         }
+//       }
+//     ],
+//     hitsPerPage: 20,
+//     index: 'vdf',
+//     nbHits: 2,
+//     nbPages: 50,
+//     page: 0,
+//     params: '',
+//     processingTimeMS: 1,
+//     query: 'a'
+//   }]
+// }
+
+const result2 = {
+  'results': [
+    {
+      'hits': [
+        {
+          'name': 'Sony - PlayStation 3 The Last of Us Bundle - 500GB',
+          '_highlightResult': {
+            'name': {
+              'value': 'Sony - __ais-highlight__PlayStation__/ais-highlight__ 3 The Last of __ais-highlight__Us__/ais-highlight__ Bundle - __ais-highlight__500GB__/ais-highlight__',
+            }
+          }
+        }
+      ],
+      'facets': {
+        'sport': {
+          'MTB': 1
+        }
+      },
+      'nbHits': 1,
+      'page': 0,
+      'nbPages': 1,
+      'hitsPerPage': 20,
+      'processingTimeMS': 3,
+      'exhaustiveNbHits': true,
+      'query': 'playstation4 (500gb) us ',
+      'params': 'query=playstation4%20(500gb)%20us%20&page=0&highlightPreTag=__ais-highlight__&highlightPostTag=__%2Fais-highlight__&facets=%5B%5D&tagFilters=',
+      'index': 'vdf'
+    }
+  ]
+}
+
+const client = {
+  search (requests) {
+    return Promise.resolve(result2)
+  }
+}
+
+const store = createFromAlgoliaClient(client)
+
+// store.addFacet('sport')
+
 export default {
   data: () => ({
     drawer: null,
-    searchClient: {
-      search (requests) {
-        return 'A'
-      },
-      searchForFacetValues (requests) {
-        return 'B'
-      }
-    }
+    searchStore: store
   })
 }
 </script>
