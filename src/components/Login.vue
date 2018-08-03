7817365d8f7bda294ee68312c7bd1e8728f60fd8
Georgian Grec <georgian.grec@gmail.com>
2018-08-04 01:08:37 +0300

    Trying to fix CORS issues

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,27 +5,19 @@
 </template>
 <script>
 import axios from 'axios'
-// Exported in a shared file
-const myLoginRoutine = user => new Promise((resolve, reject) => {
-  axios({ url: '/api/auth/facebook', data: user, method: 'GET' })
-    .then(resp => {
-      const token = resp.data.token
-      localStorage.setItem('user-token', token) // store the token in localstorage
-      resolve(resp)
-    })
-    .catch(err => {
-      localStorage.removeItem('user-token') // if the request fails, remove any possible user token if possible
-      reject(err)
-    })
-})
-
 export default {
   name: 'Login',
   methods: {
     login () {
-      const { username, password } = this
-      myLoginRoutine({ username, password }).then(() => {
-        this.$router.push('/home')
+      var config = {
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      }
+      axios.post('/api/auth/facebook', config).then(response => {
+        console.log(response)
+      }).catch(err => {
+        console.log(err)
       })
     }
   }
