a11564193aa5ba5443c7c0124ab324eb2b95c088
Georgian Grec <georgian.grec@gmail.com>
2018-08-03 18:35:09 +0300

    Trying to connect to spring app

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,14 +4,29 @@
   </div>
 </template>
 <script>
-import Vue from 'vue'
+import axios from 'axios'
+// Exported in a shared file
+const myLoginRoutine = user => new Promise((resolve, reject) => {
+  axios({ url: '/api/auth/facebook', data: user, method: 'GET' })
+    .then(resp => {
+      const token = resp.data.token
+      localStorage.setItem('user-token', token) // store the token in localstorage
+      resolve(resp)
+    })
+    .catch(err => {
+      localStorage.removeItem('user-token') // if the request fails, remove any possible user token if possible
+      reject(err)
+    })
+})
+
 export default {
   name: 'Login',
   methods: {
     login () {
-      Vue.FB.login(function (response) {
-        console.log(response)
-      }, { scope: 'email' })
+      const { username, password } = this
+      myLoginRoutine({ username, password }).then(() => {
+        this.$router.push('/home')
+      })
     }
   }
 }
