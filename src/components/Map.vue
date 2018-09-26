a5840f3dd1a123808c6e6cfb08f0fb1c64e5dd09
Georgian Grec <georgian.grec@gmail.com>
2018-09-26 11:16:25 +0300

    Playing with map markers. I don't like this fucking vue2-google-maps, because it doesn't support api v3, apparently

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -11,6 +11,8 @@
       :position="coordinateStringToObject(m.locationCoordinates)"
       :clickable="true"
       :draggable="false"
+      :icon="icon"
+      :label="labelFromEvent(m)"
     />
   </GmapMap>
 </template>
@@ -18,8 +20,26 @@
 <script>
 export default {
   name: 'vdf-map',
+  data () {
+    return {
+      icon: {
+        path: 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
+        fillColor: '#8860D0',
+        fillOpacity: 1,
+        strokeColor: '',
+        strokeWeight: 0
+      }
+    }
+  },
   props: ['vdfEvents'],
   methods: {
+    labelFromEvent: function (event) {
+      return {
+        text: event.discipline,
+        fontSize: '14px',
+        fontWeight: 'bold'
+      }
+    },
     coordinateStringToObject: function (coordinateString) {
       let split = coordinateString.split(',')
       return {lat: parseFloat(split[0]), lng: parseFloat(split[1])}
---
+++ src/components/MapJS.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+
+<script>  // TODO
+export default {
+    name: "MapJS"
+}
+</script>
+
+<style scoped>
+
+</style>
