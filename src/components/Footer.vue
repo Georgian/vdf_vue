8b04820e60ec8c54cf3d1dafcc251e8fa447574e
Georgian Grec <georgian.grec@gmail.com>
2018-09-27 23:51:25 +0300

    Playing with google maps

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <v-footer height="100px" color="primary" class="text-xs-center">
     <v-layout column>
       <v-flex mt-3 mb-1>
-        Un proiect pilot dezvoltat cu atenție, din pasiune <font-awesome-icon icon="heart"></font-awesome-icon>
+        Un proiect pilot dezvoltat în timpul liber, din pasiune pentru ciclism și programare<font-awesome-icon icon="heart"></font-awesome-icon>
       </v-flex>
       <v-flex mb-1>
         <a href="https://goo.gl/forms/IUP6RC6D4lfGVyx92" style="text-decoration: none;" class="blue--text" target="_blank">
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,14 +5,17 @@
     map-type-id="terrain"
     style="width: 100%; height: 600px"
   >
+    <GmapInfoWindow :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" @closeclick="infoWinOpen">
+      {{infoContent}}
+    </GmapInfoWindow>
+
     <GmapMarker
       :key="markerIdx"
       v-for="(m, markerIdx) in vdfEvents"
       :position="coordinateStringToObject(m.locationCoordinates)"
       :clickable="true"
       :draggable="false"
-      :icon="icon"
-      :label="labelFromEvent(m)"
+      @click="toggleInfoWindow(coordinateStringToObject(m.locationCoordinates), markerIdx)"
     />
   </GmapMap>
 </template>
@@ -22,17 +25,31 @@ export default {
   name: 'vdf-map',
   data () {
     return {
-      icon: {
-        path: 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
-        fillColor: '#8860D0',
-        fillOpacity: 1,
-        strokeColor: '',
-        strokeWeight: 0
-      }
+      infoContent: 'testasdasda',
+      infoWindowPos: null,
+      infoWinOpen: false,
+      currentMidx: null,
+      infoOptions: {}
+      // icon: {
+      //   path: 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
+      //   fillColor: '#8860D0',
+      //   fillOpacity: 1,
+      //   strokeColor: '',
+      //   strokeWeight: 0
+      // }
     }
   },
   props: ['vdfEvents'],
   methods: {
+    toggleInfoWindow: function (newPosition, idx) {
+      this.infoWindowPos = newPosition
+      if (this.currentMidx === idx) {
+        this.infoWinOpen = !this.infoWinOpen
+      } else {
+        this.infoWinOpen = true
+        this.currentMidx = idx
+      }
+    },
     labelFromEvent: function (event) {
       return {
         text: event.discipline,
